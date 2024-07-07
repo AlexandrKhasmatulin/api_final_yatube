0987f5f3ae6217f04cb89fea98a6a6da84fd93e7
AlexandrKhasmatulin <alexandr.khasmatulin@yandex.ru>
2024-07-07 15:09:01 +0300

    final

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from yatube_api.constants import MAX_TITLE_LENGTH
+from .constants import MAX_TITLE_LENGTH
 User = get_user_model()
 
 
